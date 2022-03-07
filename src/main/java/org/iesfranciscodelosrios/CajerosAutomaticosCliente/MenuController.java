475380297c5c1ea76c3b55b5188e3334326d8fa0
AngelRabasco <rabascoangel01@gmail.com>
2022-03-07 21:21:15 +0100

    Balance

--- src/main/java/org/iesfranciscodelosrios/CajerosAutomaticosCliente/MenuController.java
+++ src/main/java/org/iesfranciscodelosrios/CajerosAutomaticosCliente/MenuController.java
@@ -1,21 +1,14 @@
 package org.iesfranciscodelosrios.CajerosAutomaticosCliente;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Observable;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import model.Account;
-import model.Admin;
 import model.Client;
 import model.GesConec;
 import model.Package;
@@ -23,9 +16,7 @@ import model.Package;
 public class MenuController {
 	Client client = new Client();
 
-	ObservableList<Account> a = FXCollections.observableArrayList();
-
-	Account b = new Account();
+	ObservableList<Account> accountList = FXCollections.observableArrayList();
 
 	@FXML
 	private ListView<Account> accountlist;
@@ -47,10 +38,8 @@ public class MenuController {
 
 	@FXML
 	public void initialize() {
-
+		this.accountlist.setItems(accountList);
 		actualbalance.setText("0");
-		a.add(b);
-		accountlist.setItems(a);
 	}
 
 	@FXML
@@ -71,20 +60,28 @@ public class MenuController {
 	@FXML
 	public void substractBalance() {
 		GesConec con = new GesConec("127.0.0.1", 9999);
-		int balance = Integer.parseInt(this.balance.getText());
-		if (balance > 0 && accountlist.getSelectionModel().getSelectedItem().getBalance() > balance && con != null) {
-			accountlist.getSelectionModel().getSelectedItem()
-					.setBalance(accountlist.getSelectionModel().getSelectedItem().getBalance() - balance);
-			Package<Account> sendPackage = new Package<>();
-			sendPackage.setObject(accountlist.getSelectionModel().getSelectedItem());
-			sendPackage.setOption(13);
-			con.sendObject(sendPackage);
-			showInfoAlert("Has sacado: " + balance + " de tu cuenta");
-			actualbalance.setText(accountlist.getSelectionModel().getSelectedItem().getBalance() + "");
-		} else {
-			showErrorAlert(
-					"El dinero que quieres sacar es mayor que el que hay en la cuenta actualmente, ingrese dinero para poder retirarlo");
+
+		try {
+			Integer balance = Integer.parseInt(this.balance.getText());
+			if (balance > 0 && accountlist.getSelectionModel().getSelectedItem() != null && accountlist.getSelectionModel().getSelectedItem().getBalance() > balance) {
+
+				Package<Account> sendPackage = new Package<>();
+				sendPackage.setObject(accountlist.getSelectionModel().getSelectedItem());
+				sendPackage.setBalance(balance);
+				sendPackage.setOption(14);
+				con.sendObject(sendPackage);
+
+				accountlist.getSelectionModel().getSelectedItem().setBalance(accountlist.getSelectionModel().getSelectedItem().getBalance() - balance);
+
+				showInfoAlert("Has sacado: " + balance + " de tu cuenta");
+				actualbalance.setText(accountlist.getSelectionModel().getSelectedItem().getBalance() + "");
+			} else {
+				showErrorAlert("El dinero que quieres sacar es mayor que el que hay en la cuenta actualmente, ingrese dinero para poder retirarlo");
+			}
+		} catch (NumberFormatException e) {
+			showErrorAlert("'" + this.balance.getText() + "'" + " no es un valor válido");
 		}
+
 	}
 
 	/**
@@ -96,24 +93,34 @@ public class MenuController {
 	@FXML
 	public void addBalance() {
 		GesConec con = new GesConec("127.0.0.1", 9999);
-		int balance = Integer.parseInt(this.balance.getText());
 
-		if (balance > 0 && accountlist.getSelectionModel().getSelectedItem() != null) {
-			accountlist.getSelectionModel().getSelectedItem()
-					.setBalance(accountlist.getSelectionModel().getSelectedItem().getBalance() + balance);
-			Package<Account> sendPackage = new Package<>();
-			sendPackage.setObject(accountlist.getSelectionModel().getSelectedItem());
-			sendPackage.setOption(14);
-			con.sendObject(sendPackage);
-			actualbalance.setText(accountlist.getSelectionModel().getSelectedItem().getBalance() + "");
-			showInfoAlert("Has introducido: " + balance + " a tu cuenta");
-		} else {
-			showErrorAlert("Error al introducir dinero");
+		try {
+			Integer balance = Integer.parseInt(this.balance.getText());
+			if (balance > 0 && accountlist.getSelectionModel().getSelectedItem() != null) {
+
+				Package<Account> sendPackage = new Package<>();
+				sendPackage.setObject(accountlist.getSelectionModel().getSelectedItem());
+				sendPackage.setBalance(balance);
+				sendPackage.setOption(15);
+				con.sendObject(sendPackage);
+
+				accountlist.getSelectionModel().getSelectedItem().setBalance(accountlist.getSelectionModel().getSelectedItem().getBalance() + balance);
+
+				showInfoAlert("Has introducido: " + balance + " a tu cuenta");
+				actualbalance.setText(accountlist.getSelectionModel().getSelectedItem().getBalance() + "");
+			} else {
+				showErrorAlert("El dinero que quieres sacar es mayor que el que hay en la cuenta actualmente, ingrese dinero para poder retirarlo");
+			}
+		} catch (NumberFormatException e) {
+			showErrorAlert("'" + this.balance.getText() + "'" + " no es un valor válido");
 		}
+
 	}
 
 	public void loadUser(@SuppressWarnings("exports") Client client) {
+		System.out.println(client.getAccounts());
 		this.client = client;
+		this.accountList.setAll(client.getAccounts());
 	}
 
 	/**
